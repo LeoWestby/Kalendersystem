1417b8fba404b02c939d1f58bc9abd80c5f1e60e
Victor Fielding <o.no.manual@gmail.com>
2012-03-05 11:53:43 +0100

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Kristian tester commit");
+		System.out.println("Victor tester commit og push. ");
 	}
 }
