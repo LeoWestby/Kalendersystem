4fd1a352df1da9838699e45fea73464b48e64ff3
PlainBixit <kristian_snare@hotmail.com>
2012-03-05 16:14:39 +0100

    Test i Indigo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Kristian tester commit");
+		System.out.println("Kristian tester push");
 		System.out.println("Victor tester commit og push. ");
 		System.out.println("TT :)");
 	}
