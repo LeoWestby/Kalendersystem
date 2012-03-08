7612b62160fb7db199e8507c4a42e4e6e38b3636
Leo Martin Westby <leo.priv@gmail.com>
2012-03-08 09:35:00 +0100

    More attempts to get ConnectionImpl to work

--- src/no/ntnu/fp/net/co/ConnectionImpl.java
+++ src/no/ntnu/fp/net/co/ConnectionImpl.java
@@ -12,10 +12,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import no.ntnu.fp.net.cl.ClException;
+import no.ntnu.fp.net.admin.Log;
 import no.ntnu.fp.net.cl.ClSocket;
 import no.ntnu.fp.net.cl.KtnDatagram;
-import no.ntnu.fp.net.co.AbstractConnection.State;
+import no.ntnu.fp.net.cl.KtnDatagram.Flag;
 
 /**
  * Implementation of the Connection-interface. <br>
@@ -37,20 +37,19 @@ public class ConnectionImpl extends AbstractConnection {
 			.synchronizedMap(new HashMap<Integer, Boolean>());
 
 	/**
-	 * Initialise initial sequence number and setup state machine.
+	 * Initializes initial sequence number and setup state machine.
 	 * 
 	 * @param myPort
 	 *            - the local port to associate with this connection
 	 */
 	public ConnectionImpl(int myPort) {
-		super();
+		super(); //Initializes sequence number and sets state to disabled
 		myAddress = getIPv4Address();
 		this.myPort = myPort;
-		//throw new UnsupportedOperationException("Method not implemented");
-
+		usedPorts.put(myPort, true);
 	}
-
-	private String getIPv4Address() {
+	
+	private static String getIPv4Address() {
 		try {
 			return InetAddress.getLocalHost().getHostAddress();
 		} catch (UnknownHostException e) {
@@ -75,10 +74,62 @@ public class ConnectionImpl extends AbstractConnection {
 			throws IOException, SocketTimeoutException {
 		this.remoteAddress = remoteAddress.getHostAddress();
 		this.remotePort = remotePort;
-		state = State.ESTABLISHED;
-		//throw new UnsupportedOperationException("Method not implemented");
-	}
+		
+        if (state != State.CLOSED) {
+            throw new ConnectException("The socket is already connected");
+        }
+        Log.writeToLog("Trying to connect to: "
+							+ remoteAddress.getHostAddress() 
+							+ " : " 
+							+ remotePort, 
+							"ConnectionImpl");
+		//Handshake
+		try {
+			//Send SYN to server
+			int tries = 3;
+			boolean sent = false;
 
+			KtnDatagram synPackat = constructInternalPacket(Flag.SYN);
+
+			// Send the SYN, trying at most `tries' times.
+			Log.writeToLog(synPackat, "Sending SYN", "ConnectionImpl");
+
+			do {
+				try {
+					new ClSocket().send(synPackat);
+					sent = true;
+				} catch (ConnectException e) {
+					// Silently ignore: Maybe server was processing and didn't
+					// manage to receive the syn before we were ready to send.
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException ex) {/*Ignore*/}
+				}
+			} while (!sent && (tries-- > 0));
+
+			if (!sent) {
+				nextSequenceNo--;
+				throw new ConnectException("Unable to send SYN.");
+			}
+			state = State.SYN_SENT;
+			
+			//Receive SYNACK from server
+			KtnDatagram response = receiveAck(); 
+			
+			if (response == null || response.getFlag() != Flag.SYN_ACK) {
+				throw new ConnectException("No SYNACK received from server");
+			}
+			
+			//ACK the SYNACK
+			sendAck(response, false);
+		}
+		catch (Exception e) {
+			throw new ConnectException("Could not establish connection with server");
+		}
+		state = State.ESTABLISHED;
+		Log.writeToLog("Connection established", "ConnectionImpl");
+	}
+	
 	/**
 	 * Listen for, and accept, incoming connections.
 	 * 
@@ -87,8 +138,40 @@ public class ConnectionImpl extends AbstractConnection {
 	 */
 	public Connection accept() throws IOException, SocketTimeoutException {
 		state = State.LISTEN;
-		return new ConnectionImpl(myPort);
-		//throw new UnsupportedOperationException("Method not implemented");
+		KtnDatagram response = null;
+		Log.writeToLog("Listening for new connections on port " + myPort, 
+						"ConnectionImpl");
+		
+		while (!isValid(response) || response.getFlag() != Flag.SYN) {
+			//Wait for SYN package
+			response = receivePacket(true);
+		}
+		state = State.SYN_RCVD;
+		remoteAddress = response.getSrc_addr();
+		remotePort = response.getSrc_port();
+		
+		//Send SYNACK
+		sendAck(response, true);
+
+		if (!isValid(receiveAck())) {
+			throw new ConnectException(
+					"Three-way handshake with incoming connection failed");
+		}
+		
+		//Return the established connection to client
+		state = State.ESTABLISHED;
+		return new ConnectionImpl(getFreePort());
+    }
+	
+	/**
+	 * @return An unused, random port between 10k and 20k.
+	 */
+	private static int getFreePort() {
+		int port;
+		while (usedPorts.containsKey(
+				port = (int)(Math.random() * 10000) + 10000))
+			;
+		return port;
 	}
 
 	/**
@@ -104,12 +187,10 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @see no.ntnu.fp.net.co.Connection#send(String)
 	 */
 	public void send(String msg) throws ConnectException, IOException {
-		KtnDatagram packet = constructDataPacket(msg);
-		try {
-			if (packet == null) return;
-			simplySendPacket(packet);
-		} catch (ClException e) {e.printStackTrace();}
-		//throw new UnsupportedOperationException("Method not implemented");
+		if (state != State.ESTABLISHED) {
+			throw new ConnectException("Cannot send without an established connection");
+		}
+		sendDataPacketWithRetransmit(constructDataPacket(msg));
 	}
 
 	/**
@@ -121,8 +202,11 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @see AbstractConnection#sendAck(KtnDatagram, boolean)
 	 */
 	public String receive() throws ConnectException, IOException {
+		if (state != State.ESTABLISHED) {
+			throw new ConnectException("Cannot receive without an established connection");
+		}
 		return (String)receivePacket(false).getPayload();
-		//throw new UnsupportedOperationException("Method not implemented");
+		//TODO: Handle potential connection errors
 	}
 
 	/**
@@ -131,8 +215,8 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @see Connection#close()
 	 */
 	public void close() throws IOException {
-		
-		//throw new UnsupportedOperationException("Method not implemented");
+		//TODO: Send FIN package
+		state = State.CLOSED;
 	}
 
 	/**
@@ -144,6 +228,7 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @return true if packet is free of errors, false otherwise.
 	 */
 	protected boolean isValid(KtnDatagram packet) {
-		return packet.calculateChecksum() == packet.getChecksum();
+		return packet != null;
+		//return packet != null && packet.calculateChecksum() == packet.getChecksum();
 	}
 }
