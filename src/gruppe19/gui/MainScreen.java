1d94ac8d7848b9301139a6428d9bbd6ce93bf3a2
Leo Martin Westby <leo.priv@gmail.com>
2012-03-19 00:47:12 +0100

    Calendar improvements.

--- src/gruppe19/gui/MainScreen.java
+++ src/gruppe19/gui/MainScreen.java
@@ -3,9 +3,18 @@ package gruppe19.gui;
 import gruppe19.model.Appointment;
 import gruppe19.model.User;
 
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -20,7 +29,13 @@ public class MainScreen extends JFrame {
 	private final MainMenu menu;
 	private final JCalendar miniCalendar;
 	private final CalendarView calendar;
-	public final JLabel calendarHeader;
+	private final JButton logOut;
+	
+	private final JLabel calendarHeader;
+	private final JLabel leftArrow;
+	private final JLabel rightArrow;
+	private final JLabel week;
+	private final JLabel selectWeek;
 	
 	private class MainMenu extends JPanel {
 		private final int XPOS = 47;
@@ -74,27 +89,102 @@ public class MainScreen extends JFrame {
 		setSize(1280, 720);
 		
 		loggedInUser = user;
-		calendarHeader = new JLabel(new ImageIcon(getClass().getResource("/gruppe19/client/images/CalendarHead.png")));
-		calendarHeader.setSize(calendarHeader.getPreferredSize());
-		calendar = new CalendarView(this);
 		menu = new MainMenu();
 		miniCalendar = new JCalendar();
+		calendar = new CalendarView(new Date());
+		logOut = new JButton("Logg ut");
+		selectWeek = new JLabel("Velg uke:");
+		week = new JLabel("Uke " + calendar.getCurrentWeek());
+		leftArrow = new JLabel(new ImageIcon(getClass().getResource("/gruppe19/client/images/LeftArrow.png")));
+		rightArrow = new JLabel(new ImageIcon(getClass().getResource("/gruppe19/client/images/RightArrow.png")));
+		calendarHeader = new JLabel(new ImageIcon(getClass().getResource("/gruppe19/client/images/CalendarHead.png")));
+		
+		week.setFont(new Font(Font.SERIF, Font.BOLD, 26));
+		selectWeek.setFont(new Font(Font.SERIF, Font.BOLD, 22));
+		
+		leftArrow.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		rightArrow.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-		calendarHeader.setLocation(calendar.getX() + 1, 0);
-		add(calendarHeader);
+		miniCalendar.setSize(250, 250);
+		leftArrow.setSize(leftArrow.getPreferredSize());
+		rightArrow.setSize(rightArrow.getPreferredSize());
+		logOut.setSize(logOut.getPreferredSize());
+		week.setSize(week.getPreferredSize());
+		selectWeek.setSize(selectWeek.getPreferredSize());
+		calendarHeader.setSize(calendarHeader.getPreferredSize());
+		
+		miniCalendar.setLocation(20, getHeight() - 300);
+		selectWeek.setLocation(miniCalendar.getX(), miniCalendar.getY() - 40);
+		
+		setLocationsAndSizes();
+		
+		add(miniCalendar);
+		add(selectWeek);
+		add(week);
+		add(logOut);
 		add(menu);
+		add(calendarHeader);
 		add(calendar);
+		add(leftArrow);
+		add(rightArrow);
 		
 		setTitle("Kalendersystem - Hovedskjerm");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		
+		//Called after the window is resized
 		getContentPane().addComponentListener(new ComponentAdapter() {
 			@Override
 			public void componentResized(ComponentEvent e) {
-				calendar.setPos();
-				calendarHeader.setLocation(calendar.getX() + 1, 0);
+				setLocationsAndSizes();
+			}
+		});
+		
+		//Called every time the mini calendar is clicked
+		miniCalendar.addPropertyChangeListener(new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				if (evt.getPropertyName().equals("calendar")) {
+					calendar.setDate(miniCalendar.getDate());
+					week.setText("Uke " + calendar.getCurrentWeek());
+				}
+			}
+		});
+		
+		//Called when the left arrow is clicked
+		leftArrow.addMouseListener(new MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent e) {
+				GregorianCalendar cal = new GregorianCalendar();
+				
+				cal.setTime(calendar.getDate());
+				cal.add(Calendar.WEEK_OF_YEAR, -1);
+				
+				calendar.setDate(cal.getTime());
+				miniCalendar.setDate(calendar.getDate());
+				week.setText("Uke " + calendar.getCurrentWeek());
+			}
+		});
+		
+		//Called when the right arrow is clicked
+		rightArrow.addMouseListener(new MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent e) {
+				GregorianCalendar cal = new GregorianCalendar();
+				
+				cal.setTime(calendar.getDate());
+				cal.add(Calendar.WEEK_OF_YEAR, 1);
+				
+				calendar.setDate(cal.getTime());
+				miniCalendar.setDate(calendar.getDate());
+				week.setText("Uke " + calendar.getCurrentWeek());
+			}
+		});
+		
+		//Called when logout is clicked
+		logOut.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MainScreen.this.dispose();
 			}
 		});
 		
@@ -119,6 +209,33 @@ public class MainScreen extends JFrame {
 		calendar.addAppointment(a2);
 	}
 	
+	/**
+	 * Sets all non-constant location and size attributes of all the
+	 *  elements in the JFrame according to the current frame size.
+	 */
+	private void setLocationsAndSizes() {
+		logOut.setLocation(this.getWidth() - logOut.getWidth() - 20, 20);
+		
+		calendar.setSize(calendarHeader.getWidth() + 18, 
+							this.getHeight() - 140);
+		
+		calendar.setLocation(this.getWidth() - calendar.getWidth() - 20, 
+								calendarHeader.getHeight() +
+								week.getHeight() + logOut.getHeight() + 10);
+		
+		week.setLocation(calendar.getX() + 
+							calendarHeader.getWidth() / 2 -
+							week.getWidth() / 2, 30);
+		
+		calendarHeader.setLocation(calendar.getX() + 1, 
+									week.getHeight() + logOut.getHeight() + 10);
+		
+		leftArrow.setLocation(week.getX() - leftArrow.getWidth() - 40, 
+								week.getY());
+		
+		rightArrow.setLocation(week.getX() + week.getWidth() + 40, 
+								week.getY());
+	}
 	
 	public static void main(String[] args) {
 		new MainScreen(new User("Hans", "Hansen"));
